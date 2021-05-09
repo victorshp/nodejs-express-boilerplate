43e5b9dfd7387c2a60e94c8347ced1d3fa7378df
Victor Park <victor.park@zenvia.com>
2021-05-09 02:32:10 -0300

    feat: environment variables set and nodemon working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hey world!');
+import express from 'express';
+require('dotenv').config();
+
+const port = process.env.APP_PORT || 3000;
+const app = express();
+app.listen(port, () => {
+  console.log(`Port open @ ${port}`);
+});
