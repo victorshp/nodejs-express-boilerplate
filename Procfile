85f815ee10de14fb2e602a54c2e4abcc7f0952ff
Victor Park <victor.park@zenvia.com>
2021-05-13 22:34:46 -0300

    feat: setup finished for Heroku easy deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: node dist/src/index.js
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 require('dotenv').config();
 
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.listen(port, () => {
   console.log(`Port open @ ${port}`);
